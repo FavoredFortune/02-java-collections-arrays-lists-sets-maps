33019ec4a5773ae408074fff092fe4bcf153b6b8
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-11 13:20:09 -0700

    finshing touches on roll function complete and duplicate function set up for work

---
+++ src/ContainsDuplicates.java
@@ -0,0 +1,11 @@
+public class ContainsDuplicates {
+
+    public static void main(String[] args) {
+        //introducing the program to a user with an overview of purpose and expected output
+        String greeting = "Welcome to this roll the dice Java program \nthat will roll a 6-sided dice the number of times\nand produce an array with each roll result.\n";
+        System.out.println(greeting);
+    }
+
+
+    System.out.println("NOTE: When this method works, the last thing you'll see is \n \"Process finished with exit code 0\" (because 0 = true in JAVA).");
+}
